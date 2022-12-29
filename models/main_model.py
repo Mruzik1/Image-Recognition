d7f4d786feef1aae9dd8b5e7938c54e9a4b9af2d
Mruzik1 <mimisha2014@gmail.com>
2022-12-29 01:49:25 +0100

    improved methods structure in the MainModel

--- models/main_model.py
+++ models/main_model.py
@@ -1,6 +1,6 @@
 import torch
 from torch.utils.data import DataLoader
-from torch import nn
+from torch import nn, Tensor
 
 from .classifier import Classifier
 from .conv import ConvolutionalLayers
@@ -24,17 +24,17 @@ class MainModel:
 
     # getting training history
     @property
-    def history(self) -> list[torch.Tensor]:
+    def history(self) -> list[Tensor]:
         return self.__train_history
     
     # print training process
-    def __print_process(self, loss: float, batch: int, epoch: int, epochs_total: int):
+    def __print_process(self, loss: Tensor, batch: int, epoch: int, epochs_total: int):
         if (batch == 0):
             print('')
         print(f'Epoch: [{epoch+1}/{epochs_total}] | Batch: {batch} | Batch Loss: [{loss:.4f}]', end='\r') 
 
     # testing loop
-    def test_loop(self, dataset: DataLoader) -> tuple[torch.Tensor]:
+    def test_loop(self, dataset: DataLoader) -> tuple[Tensor]:
         with torch.inference_mode():
             self.__model.eval()
             features, labels = next(iter(dataset))
@@ -47,6 +47,23 @@ class MainModel:
 
         self.__model.train()
         return pred.detach(), loss.detach()
+
+    # training step (one epoch)
+    def __train_step(self, epoch: int, epochs: int, dataset: DataLoader, total_loss: int) -> Tensor:
+        for batch, (features, labels) in enumerate(dataset):
+            features.to(self.__device)
+            labels.to(self.__device)
+            
+            pred = self.__model(features)
+            loss = self.__loss_fn(pred, labels)
+            total_loss += loss.detach()
+            
+            self.__optimizer.zero_grad()
+            loss.backward()
+            self.__optimizer.step()
+
+            self.__print_process(total_loss/len(dataset), batch, epoch, epochs)
+        return total_loss/len(dataset)
     
     # training loop
     def train_loop(self, epochs: int, dataset: DataLoader):
@@ -54,22 +71,7 @@ class MainModel:
         print('Started Training...')
 
         for epoch in range(epochs):
-            total_loss = 0
-            for batch, (features, labels) in enumerate(dataset):
-                features.to(self.__device)
-                labels.to(self.__device)
-                
-                pred = self.__model(features)
-                loss = self.__loss_fn(pred, labels)
-                with torch.no_grad():
-                    total_loss += loss.detach()
-                
-                self.__optimizer.zero_grad()
-                loss.backward()
-                self.__optimizer.step()
-
-                self.__print_process(total_loss/len(dataset), batch, epoch, epochs)
+            total_loss = self.__train_step(epoch, epochs, dataset, 0)
             
-            print(f'\nTotal Epoch Loss: {total_loss/len(dataset):.4f}')
-            self.__train_history.append(total_loss/len(dataset))
-            
+            print(f'\nTotal Epoch Loss: {total_loss:.4f}')
+            self.__train_history.append(total_loss)
